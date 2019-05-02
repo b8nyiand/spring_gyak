c01f02ceefef4011be648522f6d627b3e2fdb154
Nyilas András <andras.nyilas@gmail.com>
2019-05-02 02:05:34 +0200

    configuration edits

--- todo_list/src/main/java/hu/flowacademy/todolist/TodolistApplication.java
+++ todo_list/src/main/java/hu/flowacademy/todolist/TodolistApplication.java
@@ -1,9 +1,11 @@
 package hu.flowacademy.todolist;
 
 import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.EnableAutoConfiguration;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
 @SpringBootApplication
+@EnableAutoConfiguration
 public class TodolistApplication {
 
 	public static void main(String[] args) {
